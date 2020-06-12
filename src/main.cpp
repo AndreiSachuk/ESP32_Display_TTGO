52d83077ab37839fdf057958d5c744f8d6fa6ed0
worldofdeveloper <developer.brest@gmail.com>
2020-06-12 22:10:22 +0300

    Пакет принимается

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,62 @@
 #include <Button2.h>
 #include "esp_adc_cal.h"
 #include "bmp.h"
+#include <SoftwareSerial.h>
+
+//автотаймер через какое время, что выполнять EXEC_TIMER_SET(Tafter,Tcode)
+#define COMBINE1(X,Y) X##Y  // helper macro
+#define COMBINE(X,Y) COMBINE1(X,Y)
+#define EXEC_TIMER_SET_NM(Tname,Tafter,Tcode) \
+{                                             \
+  static uint32_t Tname = millis()+Tafter;    \
+  if(Tname < millis()){                       \
+    Tname = millis()+Tafter;                  \
+    {Tcode}                                   \
+  }                                           \
+}
+#define EXEC_TIMER_SET(Tafter,Tcode) EXEC_TIMER_SET_NM(COMBINE(__FUNCTION__,__LINE__),Tafter,Tcode)
+
+
+//Конвертнуть из строки в бул
+bool toBool(String s);
+
+//Конвертим #****** в цвета
+#define getColor(r,g,b) (r|g<<8|b<<16)
+#define getColorR(v) (v&0xFF)
+#define getColorG(v) ((v>>8)&0xFF)
+#define getColorB(v) ((v>>16)&0xFF)
+
+uint32_t hexColorToInt(String s);
+
+//Вывести в сериал переменную LOG_DEBUG_VARIABLE(VAR_NAME)
+#define LOG_DEBUG_VARIABLE(VAR_NAME) {Serial.println(String(#VAR_NAME)+":"+String(VAR_NAME));}
+
+//ретурнуть последнюю измененную переменную из этих двух, указав их тип
+#define GET_LAST_CHANGED(TYPE_NAME, IN_X1, IN_X2)\
+([](TYPE_NAME inX1, TYPE_NAME inX2) -> TYPE_NAME {\
+  static TYPE_NAME x1 = inX1;\
+  static TYPE_NAME x2 = inX2;\
+  static TYPE_NAME def = inX1;\
+\
+  if(x1 != inX1){\
+    def = inX1;\
+    x1 = inX1;\
+    return inX1;\
+  }\
+  if(x2 != inX2){\
+    def = inX2;\
+    x2 = inX2;\
+    return inX2;\
+  }\
+\
+  return def;\
+})(IN_X1, IN_X2);
+
+#define RX 22                        
+#define TX 17 
+
+
+char data[7]; 
 
 #ifndef TFT_DISPOFF
 #define TFT_DISPOFF 0x28
@@ -28,130 +84,31 @@
 #define BUTTON_2        0
 
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
-Button2 btn1(BUTTON_1);
-Button2 btn2(BUTTON_2);
 
 char buff[512];
 int vref = 1100;
 int btnCick = false;
+byte k;
 
-//! Long time delay, it is recommended to use shallow sleep, which can effectively reduce the current consumption
-void espDelay(int ms)
-{
-    esp_sleep_enable_timer_wakeup(ms * 1000);
-    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
-    esp_light_sleep_start();
-}
+uint8_t Red, Green, Blue, Single;
 
-void showVoltage()
-{
-    static uint64_t timeStamp = 0;
-    if (millis() - timeStamp > 1000) {
-        timeStamp = millis();
-        uint16_t v = analogRead(ADC_PIN);
-        float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
-        String voltage = "Voltage :" + String(battery_voltage) + "V";
-        Serial.println(voltage);
-        tft.fillScreen(TFT_BLACK);
-        tft.setTextDatum(MC_DATUM);
-        tft.drawString(voltage,  tft.width() / 2, tft.height() / 2 );
-    }
-}
-
-void wifi_scan()
-{
-    tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.fillScreen(TFT_BLACK);
-    tft.setTextDatum(MC_DATUM);
-    tft.setTextSize(1);
-
-    tft.drawString("Scan Network", tft.width() / 2, tft.height() / 2);
-
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
-
-    int16_t n = WiFi.scanNetworks();
-    tft.fillScreen(TFT_BLACK);
-    if (n == 0) {
-        tft.drawString("no networks found", tft.width() / 2, tft.height() / 2);
-    } else {
-        tft.setTextDatum(TL_DATUM);
-        tft.setCursor(0, 0);
-        Serial.printf("Found %d net\n", n);
-        for (int i = 0; i < n; ++i) {
-            sprintf(buff,
-                    "[%d]:%s(%d)",
-                    i + 1,
-                    WiFi.SSID(i).c_str(),
-                    WiFi.RSSI(i));
-            tft.println(buff);
-        }
-    }
-    WiFi.mode(WIFI_OFF);
-}
-
-void button_init()
-{
-    btn1.setLongClickHandler([](Button2 & b) {
-        btnCick = false;
-        int r = digitalRead(TFT_BL);
-        tft.fillScreen(TFT_BLACK);
-        tft.setTextColor(TFT_GREEN, TFT_BLACK);
-        tft.setTextDatum(MC_DATUM);
-        tft.drawString("Press again to wake up",  tft.width() / 2, tft.height() / 2 );
-        espDelay(6000);
-        digitalWrite(TFT_BL, !r);
-
-        tft.writecommand(TFT_DISPOFF);
-        tft.writecommand(TFT_SLPIN);
-        //After using light sleep, you need to disable timer wake, because here use external IO port to wake up
-        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_TIMER);
-        // esp_sleep_enable_ext1_wakeup(GPIO_SEL_35, ESP_EXT1_WAKEUP_ALL_LOW);
-        esp_sleep_enable_ext0_wakeup(GPIO_NUM_35, 0);
-        delay(200);
-        esp_deep_sleep_start();
-    });
-    btn1.setPressedHandler([](Button2 & b) {
-        Serial.println("Detect Voltage..");
-        btnCick = true;
-    });
-
-    btn2.setPressedHandler([](Button2 & b) {
-        btnCick = false;
-        Serial.println("btn press wifi scan");
-        wifi_scan();
-    });
-}
-
-void button_loop()
-{
-    btn1.loop();
-    btn2.loop();
-}
-
+  
+           
 
 void setup()
 {
     Serial.begin(115200);
+    Serial2.begin(9600,SERIAL_8N1,RX,TX);
     Serial.println("Start");
 
-    /*
-    ADC_EN is the ADC detection enable port
-    If the USB port is used for power supply, it is turned on by default.
-    If it is powered by battery, it needs to be set to high level
-    */
-    pinMode(ADC_EN, OUTPUT);
-    digitalWrite(ADC_EN, HIGH);
-
     tft.init();
-    tft.setRotation(1);
+    tft.setRotation(0);
     tft.fillScreen(TFT_BLACK);
     tft.setTextSize(2);
     tft.setTextColor(TFT_GREEN);
     tft.setCursor(0, 0);
     tft.setTextDatum(MC_DATUM);
-    tft.setTextSize(1);
+    tft.setTextSize(2);
 
     if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
         pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
@@ -162,41 +119,42 @@ void setup()
 
     tft.setRotation(0);
     tft.fillScreen(TFT_RED);
-    espDelay(1000);
+    delay(300);
     tft.fillScreen(TFT_BLUE);
-    espDelay(1000);
+    delay(300);
     tft.fillScreen(TFT_GREEN);
-    espDelay(1000);
-
-    button_init();
-
-    esp_adc_cal_characteristics_t adc_chars;
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
-    //Check type of calibration value used to characterize ADC
-    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
-        Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
-        vref = adc_chars.vref;
-    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
-        Serial.printf("Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
-    } else {
-        Serial.println("Default Vref: 1100mV");
-    }
+    delay(300);
 
     tft.fillScreen(TFT_BLACK);
-    tft.setTextDatum(MC_DATUM);
-    tft.drawString("LeftButton:", tft.width() / 2, tft.height() / 2 - 16);
-    tft.drawString("[WiFi Scan]", tft.width() / 2, tft.height() / 2 );
-    tft.drawString("RightButton:", tft.width() / 2, tft.height() / 2 + 16);
-    tft.drawString("[Voltage Monitor]", tft.width() / 2, tft.height() / 2 + 32 );
-    tft.drawString("RightButtonLongPress:", tft.width() / 2, tft.height() / 2 + 48);
-    tft.drawString("[Deep Sleep]", tft.width() / 2, tft.height() / 2 + 64 );
+    tft.setTextColor(0xF800);
+    tft.drawString("RED: ", 1, 1);
+    tft.setTextColor(0x07E0);
+    tft.drawString("GREEN: ", 1, 40);
+    tft.setTextColor(0x001F);
+    tft.drawString("BLUE: ", 1, 80);
+    tft.setTextColor(0xFFFF);
+    tft.drawString("SINGLE: ", 1, 120);
     tft.setTextDatum(TL_DATUM);
 }
 
+
 void loop()
 {
-    if (btnCick) {
-        showVoltage();
-    }
-    button_loop();
+ if (Serial2.available() && Serial2.read() == 0xAA && Serial2.read() == 0xBB)
+  Serial2.readStringUntil('\n').toCharArray(data, sizeof(data));
+  LOG_DEBUG_VARIABLE((int)data[3])
+  Red = data[0];
+  Green = data[1];
+  Blue = data[2];
+  Single = data[3];
+  
+     tft.setTextColor(0xF800,0x0000);
+    tft.drawString(String(Red)+"   ", 50, 1);
+    tft.setTextColor(0x07E0,0x0000);
+    tft.drawString(String(Green)+"   ", 75, 30);
+    tft.setTextColor(0x001F,0x0000);
+    tft.drawString(String(Blue)+"   ", 60, 70);
+    tft.setTextColor(0xFFFF,0x0000);
+    tft.drawString(String(Single)+"   ", 85, 110);
+    
 }

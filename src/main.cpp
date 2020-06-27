52a41e8c176ebd85d9d3df691f325e24d7e89063
worldofdeveloper <developer.brest@gmail.com>
2020-06-27 18:28:19 +0300

    ReadMe

--- src/main.cpp
+++ src/main.cpp
@@ -10,59 +10,9 @@
 #include "leds_stat.h"
 #include <analogWrite.h>
 
-//автотаймер через какое время, что выполнять EXEC_TIMER_SET(Tafter,Tcode)
-#define COMBINE1(X,Y) X##Y  // helper macro
-#define COMBINE(X,Y) COMBINE1(X,Y)
-#define EXEC_TIMER_SET_NM(Tname,Tafter,Tcode) \
-{                                             \
-  static uint32_t Tname = millis()+Tafter;    \
-  if(Tname < millis()){                       \
-    Tname = millis()+Tafter;                  \
-    {Tcode}                                   \
-  }                                           \
-}
-#define EXEC_TIMER_SET(Tafter,Tcode) EXEC_TIMER_SET_NM(COMBINE(__FUNCTION__,__LINE__),Tafter,Tcode)
-
-
-//Конвертнуть из строки в бул
-bool toBool(String s);
-
-//Конвертим #****** в цвета
-#define getColor(r,g,b) (r|g<<8|b<<16)
-#define getColorR(v) (v&0xFF)
-#define getColorG(v) ((v>>8)&0xFF)
-#define getColorB(v) ((v>>16)&0xFF)
-
-uint32_t hexColorToInt(String s);
-
-//Вывести в сериал переменную LOG_DEBUG_VARIABLE(VAR_NAME)
-#define LOG_DEBUG_VARIABLE(VAR_NAME) {Serial.println(String(#VAR_NAME)+":"+String(VAR_NAME));}
-
-//ретурнуть последнюю измененную переменную из этих двух, указав их тип
-#define GET_LAST_CHANGED(TYPE_NAME, IN_X1, IN_X2)\
-([](TYPE_NAME inX1, TYPE_NAME inX2) -> TYPE_NAME {\
-  static TYPE_NAME x1 = inX1;\
-  static TYPE_NAME x2 = inX2;\
-  static TYPE_NAME def = inX1;\
-\
-  if(x1 != inX1){\
-    def = inX1;\
-    x1 = inX1;\
-    return inX1;\
-  }\
-  if(x2 != inX2){\
-    def = inX2;\
-    x2 = inX2;\
-    return inX2;\
-  }\
-\
-  return def;\
-})(IN_X1, IN_X2);
-
 #define RX 22                        
 #define TX 17 
 #define LED_PIN 26
-
 
 char data[7]; 
 
@@ -81,8 +31,6 @@ char data[7];
 #define TFT_RST             23
 
 #define TFT_BL          4   // Display backlight control pin
-#define ADC_EN          14  //ADC_EN is the ADC detection enable port
-#define ADC_PIN         34
 #define BUTTON_1        35
 #define BUTTON_2        0
 
@@ -105,20 +53,25 @@ void displayTest(){
     tft.setTextDatum(MC_DATUM);
     tft.setTextSize(2);
 
-    if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-        pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
-        digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+    if (TFT_BL > 0) { 
+        pinMode(TFT_BL, OUTPUT); 
+        digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); 
     }
 
     tft.setSwapBytes(true);
 
     tft.setRotation(0);
-    tft.fillScreen(TFT_RED);
-    delay(300);
     tft.fillScreen(TFT_BLUE);
     delay(300);
-    tft.fillScreen(TFT_GREEN);
-    delay(300);
+    tft.setTextColor(0x0000);
+    tft.drawString("Light", 65, 1);
+    tft.drawString("Control", 65, 40);
+    tft.drawString("System", 65, 80);
+    tft.drawString("With ", 65, 120);
+    tft.drawString("Automatic", 60, 160);
+    tft.drawString("Calibration", 40, 200);
+    delay(3000);
+
 }
 
 void displayStart(){
@@ -135,17 +88,13 @@ void displayStart(){
 }
 
 void Radio(){
- if (Serial2.available() && Serial2.read() == 0xAA && Serial2.read() == 0xBB)
-  Serial2.readStringUntil('\n').toCharArray(data, sizeof(data));
-  Red = data[0];
-  Green = data[1];
-  Blue = data[2];
-  Single = data[3];
+  if (Serial2.available() && Serial2.read() == 0xAA && Serial2.read() == 0xBB){
+    Serial2.readStringUntil('\n').toCharArray(data, sizeof(data));
+    Red = data[0];
+    Green = data[1];
+    Blue = data[2];
+    Single = data[3];}
 }
-
-
-
-
 
 void RGBprint_display(){
     tft.setTextColor(0xF800,0x0000);
@@ -156,12 +105,9 @@ void RGBprint_display(){
     tft.drawString(String(Blue)+"   ", 60, 70);
     tft.setTextColor(0xFFFF,0x0000);
     tft.drawString(String(Single)+"   ", 85, 110);
-    tft.fillRect(0, 200, 135, 40, 0xF1FF);
-    
+ 
 }
-
-
-           
+     
 
 void setup()
 {
